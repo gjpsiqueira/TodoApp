774e029f543d52ed2b3d040143709b9fbb405db5
= <gjpsiqueira@gmail.com>
2018-03-31 15:12:01 -0300

    fix default

--- backend/src/api/todo/todo.js
+++ backend/src/api/todo/todo.js
@@ -3,7 +3,7 @@ const mongoose = restful.mongoose
 
 const todoSchema = new mongoose.Schema({
     description: {type: String, require: true},
-    done: {type: Boolean, require: true, default: true},
+    done: {type: Boolean, require: true, default: false},
     createdAt: {type: Date, default: Date.now}
 })
 
