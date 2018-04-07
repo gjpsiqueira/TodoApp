3c8b2e909afa9c09fd6cd4158964f016abfc673b
= <gjpsiqueira@gmail.com>
2018-04-07 16:54:45 -0300

    Components grid, todoForm and todoList

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
 
 export default class Todo extends React.Component {
 
@@ -8,6 +11,8 @@ export default class Todo extends React.Component {
 
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"/>
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
